3c747581e1c54f0b402c995a4593eba6b859d99d
bond1q-ua <157967764+bond1q-ua@users.noreply.github.com>
2024-03-25 23:25:43 +0200

    update_comment

--- task_2.py
+++ task_2.py
@@ -2,12 +2,12 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    pattern = r"\b\d+\.\d+\b"
-    for match in re.finditer(pattern, text):
-        yield float(match.group())
+    pattern = r"\b\d+\.\d+\b"    # Визначення регулярного виразу для пошуку дійсних чисел у тексті
+    for match in re.finditer(pattern, text): # Пошук у тексті входжень дійсних чисел
+        yield float(match.group())  # Повернення знайденого числа у вигляді float через генератор
 
-def sum_profit(text: str, func: Callable):
-    total = sum(func(text))
+def sum_profit(text: str, func: Callable):  # Виклик функції для отримання ітератора дійсних чисел з тексту
+    total = sum(func(text)) #обчислення суми цих чисел
     return total
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
